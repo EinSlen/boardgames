b5bf3f6b637dccda61266920317587fa7a672b64
Valentin Damlencourt <valentin_damlencourt@ens.univ-artois.fr>
2024-07-02 15:54:50 +0200

    conflit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import {Component, NgModule} from '@angular/core';
-import { IonApp } from '@ionic/angular/standalone';
+import { IonApp, IonRouterOutlet} from '@ionic/angular/standalone';
 import {BrowserModule} from "@angular/platform-browser";
 import {SquareComponent} from "./tic-tac-toe/square/square.component";
 import {GameboardComponent} from "./tic-tac-toe/gameboard/gameboard.component";
 import {LoaderComponent} from "./loader/loader.component";
-import {IonRouterOutlet, ModalController} from '@ionic/angular';
+import { ModalController } from '@ionic/angular';
 import { RouterLink} from "@angular/router";
 
 
