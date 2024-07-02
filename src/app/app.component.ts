5a3b6d12f7a9d4dcca5d839be63885478c5332a4
Valentin Damlencourt <valentin_damlencourt@ens.univ-artois.fr>
2024-07-02 10:36:17 +0200

    #2 - Ajout du caroussel automatique

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import {BrowserModule} from "@angular/platform-browser";
+
 
 @Component({
   selector: 'app-root',
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {Component, OnInit} from '@angular/core';
+import {IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton, IonIcon} from '@ionic/angular/standalone';
+import {NgForOf, NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton, IonIcon, NgOptimizedImage, NgForOf],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  currentSlide = 0;
+  slides = [
+    { img: 'assets/img/slide1.jpg', title: 'Slide 1' },
+    { img: 'assets/img/slide2.jpg', title: 'Slide 2' },
+    { img: 'assets/img/slide3.jpg', title: 'Slide 3' },
+  ];
+  private slideInterval: any;
+
   constructor() {}
+
+  ngOnInit() {
+    this.startSlideShow();
+  }
+
+  startSlideShow() {
+    this.slideInterval = setInterval(() => {
+      this.nextSlide();
+    }, 3000);
+  }
+
+  nextSlide() {
+    this.currentSlide = (this.currentSlide + 1) % this.slides.length;
+  }
+
+  prevSlide() {
+    this.currentSlide = (this.currentSlide - 1 + this.slides.length) % this.slides.length;
+  }
 }
