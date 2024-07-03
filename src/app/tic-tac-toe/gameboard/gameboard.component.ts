4a8734752b06d109ea7ab874c931f3c96edf42e8
Ethan Copin <ethan_copin@ens.univ-artois.fr>
2024-07-03 09:07:46 +0200

    Ajout du didacticiel dans tictactoe #8

--- src/app/tic-tac-toe/gameboard/gameboard.component.ts
+++ src/app/tic-tac-toe/gameboard/gameboard.component.ts
@@ -2,19 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import {GameService} from "../../services/gameService";
 import {SquareComponent} from "../square/square.component";
 import {NgForOf} from "@angular/common";
-import {IonCol, IonGrid, IonRow} from "@ionic/angular/standalone";
+import {IonCol, IonFab, IonFabButton, IonGrid, IonIcon, IonRow} from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-gameboard',
   templateUrl: './gameboard.component.html',
   styleUrls: ['./gameboard.component.scss'],
-  imports: [
-    SquareComponent,
-    NgForOf,
-    IonGrid,
-    IonCol,
-    IonRow
-  ],
+    imports: [
+        SquareComponent,
+        NgForOf,
+        IonGrid,
+        IonCol,
+        IonRow,
+        IonFab,
+        IonFabButton,
+        IonIcon
+    ],
   standalone: true
 })
 export class GameboardComponent  implements OnInit {
--- src/app/tic-tac-toe/morpion/morpion.page.ts
+++ src/app/tic-tac-toe/morpion/morpion.page.ts
@@ -2,33 +2,44 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import {
-  IonCol,
-  IonContent,
-  IonGrid,
-  IonHeader,
-  IonRow,
-  IonTitle,
-  IonToolbar,
-  IonButton,
-  IonIcon
+    IonCol,
+    IonContent,
+    IonGrid,
+    IonHeader,
+    IonRow,
+    IonTitle,
+    IonToolbar,
+    IonButton,
+    IonIcon, IonFab, IonFabButton
 } from '@ionic/angular/standalone';
 import {GameService} from "../../services/gameService";
 import {SquareComponent} from "../square/square.component";
 import {PopupService} from "../../services/popup.service";
 import {GameboardComponent} from "../gameboard/gameboard.component";
 import {LoaderComponent} from "../../loader/loader.component";
+import {SettingsModalComponent} from "../../settings-modal/settings-modal.component";
+import {DidactModalComponent} from "../../didact-modal/didact-modal.component";
+import {ModalController} from "@ionic/angular";
+import {addIcons} from "ionicons";
+import {closeCircleOutline, helpCircleOutline, settingsOutline} from "ionicons/icons";
 
 @Component({
   selector: 'app-morpion',
   templateUrl: './morpion.page.html',
   styleUrls: ['./morpion.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, LoaderComponent, IonGrid, IonRow, IonCol, SquareComponent, IonButton, IonIcon, GameboardComponent]
+    imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, LoaderComponent, IonGrid, IonRow, IonCol, SquareComponent, IonButton, IonIcon, GameboardComponent, IonFab, IonFabButton]
 })
 export class MorpionPage implements OnInit {
   isLoading = true;
 
-  constructor(private popupService: PopupService, private gameService: GameService) {}
+  constructor(private popupService: PopupService, private gameService: GameService, private modalController: ModalController) {
+    addIcons({
+      'close-circle-outline' : closeCircleOutline,
+      'help-circle-outline' : helpCircleOutline,
+    });
+  }
+
 
   async startGame(difficulty: string) {
     const startingPlayer = await this.popupService.showStartGamePopup();
@@ -46,4 +57,13 @@ export class MorpionPage implements OnInit {
     }, 3000);
   }
 
+  async openDidactModal() {
+    const modal = await this.modalController.create({
+      component: DidactModalComponent,
+      componentProps: {
+        value: 123
+      }
+    });
+    modal.present();
+  }
 }
