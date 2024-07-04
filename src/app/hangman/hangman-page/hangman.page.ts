5e107e65a4d304d5798e34da9299f3485588e40e
Matheo Dupuis <matheodupuis1@gmail.com>
2024-07-04 09:53:33 +0200

    [#42] Règles du pendu

--- src/app/hangman/hangman-page/hangman.page.ts
+++ src/app/hangman/hangman-page/hangman.page.ts
@@ -4,35 +4,44 @@ import { FormsModule } from '@angular/forms';
 import {
   IonButton,
   IonButtons,
-  IonContent,
+  IonContent, IonFab, IonFabButton,
   IonGrid,
   IonHeader,
   IonIcon, IonRow, IonText,
   IonTitle,
   IonToolbar, Platform
 } from '@ionic/angular/standalone';
-import {NavController} from "@ionic/angular";
+import {ModalController, NavController} from "@ionic/angular";
 import {addIcons} from "ionicons";
 import {arrowBackCircleOutline, closeCircleOutline, helpCircleOutline} from "ionicons/icons";
 import {HangmanGameService} from "../../services/hangmanGame.service";
+import {DidactModalComponent} from "../../didact-modal/didact-modal.component";
 
 @Component({
   selector: 'app-hangman-page',
   templateUrl: './hangman.page.html',
   styleUrls: ['./hangman.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonButtons, IonIcon, IonGrid, IonRow, IonText]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonButtons, IonIcon, IonGrid, IonRow, IonText, IonFab, IonFabButton]
 })
 export class HangmanPage {
-  constructor(private navCtrl : NavController, protected service : HangmanGameService, private platform : Platform) {
+  constructor(private modalController : ModalController,private navCtrl : NavController, protected service : HangmanGameService, private platform : Platform) {
     addIcons({
       'close-circle-outline' : closeCircleOutline,
       'help-circle-outline' : helpCircleOutline,
-      'arrow-back-circle-outline' : arrowBackCircleOutline
+      'arrow-back-circle-outline' : arrowBackCircleOutline,
     })
   }
 
-
+  async openDidactModal() {
+    const modal = await this.modalController.create({
+      component: DidactModalComponent,
+      componentProps: {
+        gameName: 'hangman'
+      }
+    });
+    modal.present();
+  }
 
   restartGame(){
     this.platform.ready().then(() => {
